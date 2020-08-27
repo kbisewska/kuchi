99939bb0ccc5ad58aec8e9edda50975a8f72e627
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-08-27 16:36:18 +0200

    Configure WelcomeView

--- Sources/App/Welcome/WelcomeView.swift
+++ Sources/App/Welcome/WelcomeView.swift
@@ -34,7 +34,35 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("welcome-background", bundle: nil)
+                .resizable()
+                .aspectRatio(1 / 1, contentMode: .fill)
+                .blur(radius: 5)
+                .opacity(0.08)
+            
+            HStack {
+                Image(systemName: "table")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    .background(Color(white: 0.9))
+                    .clipShape(Circle()) .foregroundColor(.red)
+                
+                VStack(alignment: .leading) {
+                    Text("Welcome to")
+                        .font(.headline)
+                        .bold()
+                    Text("Kuchi")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
